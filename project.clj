2d21c8b1e569b39f1d70861f6f033a204ee79873
Roman <roman@burningswell.com>
2012-02-21 22:22:44 +0100

    Added :author and :url to project.clj.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject geo-clj "0.0.1-SNAPSHOT"
+  :author "Roman Scherer"
   :description "An experimental Clojure geo library."
+  :url "http://github.com/r0man/geo-clj"
   :dependencies [[org.clojure/clojure "1.3.0"]])
