df8e2e479429f5b17b2224da5d65dafaa628fa66
Roman <roman@burningswell.com>
2012-04-16 19:57:38 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject geo-clj/geo-clj "0.0.3-SNAPSHOT"
   :description "An experimental Clojure geo library."
-  :dependencies [[org.clojure/clojure "1.4.0-beta4"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :author "Roman Scherer"
   :url "http://github.com/r0man/geo-clj"
   :min-lein-version "2.0.0")
