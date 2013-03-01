168ed95a988b56293f5804cc81754f26750878c7
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 17:21:02 +0100

    Release 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geo-clj "0.1.1-SNAPSHOT"
+(defproject geo-clj "0.2.0"
   :description "Geographic encoding/decoding for Clojure and ClojureScript)."
   :url "http://github.com/r0man/geo-clj"
   :author "Roman Scherer"
