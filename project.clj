d11e07f725f55832d96080c4454e81d8c6ac9d73
Roman Scherer <roman.scherer@burningswell.com>
2013-10-12 19:11:51 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -5,13 +5,14 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.1.5"]
+  :lein-release {:deploy-via :clojars}
+  :dependencies [[noencore "0.1.6"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1909"]
+                 [org.clojure/clojurescript "0.0-1934"]
                  [org.postgis/postgis-jdbc "1.3.3"]]
-  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]
-                                  [com.keminglabs/cljx "0.3.0"]]
-                   :plugins [[com.cemerick/austin "0.1.1"]]
+  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.0"]]
+                   :plugins [[com.cemerick/austin "0.1.1"]
+                             [com.cemerick/clojurescript.test "0.1.0"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}}
   :plugins [[com.keminglabs/cljx "0.3.0"]
             [lein-cljsbuild "0.3.3"]]
@@ -28,11 +29,9 @@
                   {:source-paths ["test"]
                    :output-path "target/test-classes"
                    :rules :cljs}]}
-  :cljsbuild {:test-commands {"phantom" ["runners/phantomjs.js" "target/testable.js"]}
+  :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
               :builds [{:source-paths ["target/classes" "target/test-classes"]
                         :compiler {:output-to "target/testable.js"
-                                   :libs [""]
                                    :optimizations :advanced
                                    :pretty-print true}}]}
-  :test-paths ["target/test-classes"]
-  :lein-release {:deploy-via :clojars})
+  :test-paths ["target/test-classes"])
