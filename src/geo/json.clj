20af7df1fda181c2f57df7f00cb34e95802efdb1
Roman Scherer <roman@burningswell.com>
2014-07-12 23:46:37 +0200

    Add multi line string json encoder.

--- src/geo/json.clj
+++ src/geo/json.clj
@@ -7,22 +7,21 @@
            (java.io PrintWriter)))
 
 (defn encode-line-string
-  "Encode `line-string` into a GeoJSON compatible data structure."
-  [line-string]
-  {:type "LineString" :coordinates (coordinates line-string)})
+  "Encode `geom` into a GeoJSON compatible line string data
+  structure."
+  [geom]
+  {:type "LineString" :coordinates (coordinates geom)})
+
+(defn encode-multi-line-string
+  "Encode `geom` into a GeoJSON compatible multi line string data
+  structure."
+  [geom]
+  {:type "MultiLineString" :coordinates (coordinates geom)})
 
 (defn encode-point
-  "Encode `point` into a GeoJSON compatible data structure."
+  "Encode `point` into a GeoJSON compatible point data structure."
   [point]
   {:type "Point" :coordinates (coordinates point)})
-
-(extend-type org.postgis.Point
-  JSONable
-  (to-json [geom ^JsonGenerator generator]
-    (encode-map (encode-point geom) generator))
-  JSONWriter
-  (-write [geom ^PrintWriter writer]
-    (write (encode-point geom) writer)))
 
 (extend-type org.postgis.LineString
   JSONable
@@ -31,3 +30,19 @@
   JSONWriter
   (-write [geom ^PrintWriter writer]
     (write (encode-line-string geom) writer)))
+
+(extend-type org.postgis.MultiLineString
+  JSONable
+  (to-json [geom ^JsonGenerator generator]
+    (encode-map (encode-multi-line-string geom) generator))
+  JSONWriter
+  (-write [geom ^PrintWriter writer]
+    (write (encode-multi-line-string geom) writer)))
+
+(extend-type org.postgis.Point
+  JSONable
+  (to-json [geom ^JsonGenerator generator]
+    (encode-map (encode-point geom) generator))
+  JSONWriter
+  (-write [geom ^PrintWriter writer]
+    (write (encode-point geom) writer)))
--- test/geo/json_test.clj
+++ test/geo/json_test.clj
@@ -11,6 +11,13 @@
            (generate-string geom)
            (json-str geom)))))
 
+(deftest test-encode-multi-line-string
+  (let [geom (multi-line-string 4326 [[10 10] [20 20] [10 40]] [[40 40] [30 30] [40 20] [30 10]])]
+    (is (= (str "{\"type\":\"MultiLineString\",\"coordinates\":[[[10.0,10.0],[20.0,20.0],[10.0,40.0]],"
+                "[[40.0,40.0],[30.0,30.0],[40.0,20.0],[30.0,10.0]]]}")
+           (generate-string geom)
+           (json-str geom)))))
+
 (deftest test-encode-point
   (let [geom (point 4326 30 10 0)]
     (is (= "{\"type\":\"Point\",\"coordinates\":[30.0,10.0,0.0]}"
