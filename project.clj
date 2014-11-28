006992a7e0b35a86c980af6844edf10269b4309e
Roman Scherer <roman@burningswell.com>
2014-11-28 17:51:50 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,43 +5,45 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :deploy-repositories [["releases" :clojars]]
   :dependencies [[cheshire "5.3.1"]
-                 [com.cognitect/transit-clj "0.8.229"]
-                 [com.cognitect/transit-cljs "0.8.137"]
-                 [noencore "0.1.16"]
+                 [com.cognitect/transit-clj "0.8.259"]
+                 [com.cognitect/transit-cljs "0.8.192"]
+                 [noencore "0.1.18"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2277" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]
                  [org.clojure/data.json "0.2.5"]
                  [org.postgis/postgis-jdbc "1.3.3"]]
   :aliases {"ci" ["do" ["difftest"] ["lint"]]
-            "lint" ["eastwood"]}
-  :cljsbuild {:builds []}
-  :test-paths ["target/test-classes"]
-  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.4.0"]]
-                   :plugins [[com.cemerick/austin "0.1.4"]
+            "lint" ["do"  ["eastwood"]]
+            "test-ancient" ["test"]}
+  :cljx {:builds [{:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  {:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :cljs}
+                  {:source-paths ["test"]
+                   :output-path "target/test-classes"
+                   :rules :clj}
+                  {:source-paths ["test"]
+                   :output-path "target/test-classes"
+                   :rules :cljs}]}
+  :cljsbuild {:test-commands {"node" ["node" :node-runner "target/testable.js"]
+                              "phantom" ["phantomjs" :runner "target/testable.js"]
+                              "rhino" ["rhino" "-opt" "-1" :rhino-runner "target/testable.js"]}
+              :builds [{:source-paths ["target/classes" "target/test-classes"]
+                        :compiler {:output-to "target/testable.js"
+                                   :optimizations :advanced
+                                   :pretty-print true}}]}
+  :deploy-repositories [["releases" :clojars]]
+  :prep-tasks [["cljx" "once"]]
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]
                              [com.cemerick/clojurescript.test "0.3.1"]
-                             [com.keminglabs/cljx "0.4.0"]
-                             [jonase/eastwood "0.1.4"]
+                             [jonase/eastwood "0.2.0"]
                              [lein-cljsbuild "1.0.3"]
-                             [lein-difftest "2.0.0"]]
-                   :hooks [cljx.hooks leiningen.cljsbuild]
-                   :cljx {:builds [{:source-paths ["src"]
-                                    :output-path "target/classes"
-                                    :rules :clj}
-                                   {:source-paths ["src"]
-                                    :output-path "target/classes"
-                                    :rules :cljs}
-                                   {:source-paths ["test"]
-                                    :output-path "target/test-classes"
-                                    :rules :clj}
-                                   {:source-paths ["test"]
-                                    :output-path "target/test-classes"
-                                    :rules :cljs}]}
-                   :cljsbuild {:test-commands {"node" ["node" :node-runner "target/testable.js"]
-                                               "phantom" ["phantomjs" :runner "target/testable.js"]}
-                               :builds [{:source-paths ["target/classes" "target/test-classes"]
-                                         :compiler {:output-to "target/testable.js"
-                                                    :optimizations :advanced
-                                                    :pretty-print true}}]}
-                   :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}})
+                             [lein-difftest "2.0.0"]
+                             [org.clojars.cemerick/cljx "0.5.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
+                   :hooks [leiningen.cljsbuild]
+                   :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
+                   :test-paths ["target/test-classes"]}
+             :test {:prep-tasks [["cljsbuild" "once"]]}})
