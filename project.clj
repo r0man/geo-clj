85c0a8fc2a8a78640623a062ef32ef563ae700d8
Roman Scherer <roman@burningswell.com>
2015-02-28 12:37:41 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.4.0"]
-                 [com.cognitect/transit-clj "0.8.259"]
-                 [com.cognitect/transit-cljs "0.8.194"]
+                 [com.cognitect/transit-clj "0.8.269"]
+                 [com.cognitect/transit-cljs "0.8.205"]
                  [noencore "0.1.18"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2511" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2913" :scope "provided"]
                  [org.clojure/data.json "0.2.5"]
                  [org.postgis/postgis-jdbc "1.3.3"]]
   :aliases {"ci" ["do" ["difftest"] ["lint"]]
@@ -36,8 +36,8 @@
                                    :pretty-print true}}]}
   :deploy-repositories [["releases" :clojars]]
   :prep-tasks [["cljx" "once"]]
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]
-                             [com.cemerick/clojurescript.test "0.3.1"]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.6-SNAPSHOT"]]
+                   :plugins [[com.cemerick/clojurescript.test "0.3.1"]
                              [jonase/eastwood "0.2.0"]
                              [lein-cljsbuild "1.0.3"]
                              [lein-difftest "2.0.0"]
